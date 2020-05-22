ee993db08a355d608eda05fc7a15a2bf8ea18e53
Dasarraju Sasidhar <iad7kor@bosch.com>
2020-05-22 13:44:37 +0530

    init version

--- get_top_results/views.py
+++ get_top_results/views.py
@@ -27,31 +27,40 @@ def home():
 @app.route('/search', methods = ["POST"])
 def search():
     details = dict(request.form)
-    print(details)
-    details = get_data(details["q"])
-    return details
-  
+    data = get_data(details["q"])
+    print(data)
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+        data = data,
+    )
 
 chromeOptions = Options()
-chromeOptions.add_argument("--kiosk")
+chromeOptions.add_argument("--headless")
+# headless  kiosk
 
 def get_data(query):
-    browser = webdriver.Chrome('C:/Users/iad7kor/Documents/Sasi/chromedriver.exe', options = chromeOptions )
-    browser.get("https://www.google.co.in/maps/")
-    time.sleep(5)
-    browser.find_element_by_id("searchboxinput").send_keys(str(query))
-    browser.find_element_by_id("searchboxinput").send_keys(Keys.ENTER)
-    time.sleep(5)
-    data = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[5]")
-    html_code =BeautifulSoup(browser.page_source, 'lxml')
-    #data_tag_code =   html_code.find_all("div", class_="section-layout section-scrollbox scrollable-y scrollable-show section-layout-flex-vertical")
-    data_tag_code =   html_code.find_all("div", class_="section-result")
-    #print(data_tag_code1)
-    data = list()
+    try:
+        browser = webdriver.Chrome('C:/Users/iad7kor/Documents/Sasi/chromedriver.exe', options = chromeOptions )
+        browser.get("https://www.google.co.in/maps/")
+        time.sleep(3)
+        browser.find_element_by_id("searchboxinput").send_keys(str(query))
+        browser.find_element_by_id("searchboxinput").send_keys(Keys.ENTER)
+        time.sleep(5)
+        data = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div")
+        html_code =BeautifulSoup(browser.page_source, 'lxml')
+        #data_tag_code =   html_code.find_all("div", class_="section-layout section-scrollbox scrollable-y scrollable-show section-layout-flex-vertical")
+        data_tag_code =   html_code.find_all("div", class_="section-result")
+        #print(data_tag_code1)
+        details = list()
+        for i in data_tag_code:
+            if(i["aria-label"]):
+                details.append(i["aria-label"])
 
-    for i in data_tag_code:
-        if(i["aria-label"]):
-            data.append(i["aria-label"])
-    
-    return data
+        return details
 
+    except Exception as e:
+            print(e)
+            details = ["details not found"]
+            return details
