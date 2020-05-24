925b6ede203545be50db408660fae23f676bf264
Dasarraju Sasidhar <dasarisasidhar.d@gmail.com>
2020-05-24 14:41:38 +0530

    using google api

--- get_top_results/views.py
+++ get_top_results/views.py
@@ -8,14 +8,12 @@ from get_top_results import app
 from flask import request
 import json
 import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-import pandas as pd
 from flask import send_file
+import googlemaps 
 
+
+gmaps = googlemaps.Client(key = "AIzaSyCFUBzYLW1sVSF2Q8laM_Sbo9JhOKqYiU0")
 @app.route('/')
 @app.route('/home')
 def home():
@@ -39,38 +37,35 @@ def search():
 
 @app.route('/download')
 def download():
-    #df = pd.DataFrame(searchresults)
-    #data = df.to_csv()
-    #path = data
     data  = ["a", "b"]
     df = pd.DataFrame(data)
     return send_file(df.to_csv(), as_attachment=True)
 
-chromeOptions = Options()
-chromeOptions.add_argument("--headless")
-# headless  kiosk
-
 def get_data(query):
     try:
-        browser = webdriver.Chrome('C:/Users/iad7kor/Documents/Sasi/chromedriver.exe', options = chromeOptions )
-        browser.get("https://www.google.co.in/maps/")
-        time.sleep(3)
-        browser.find_element_by_id("searchboxinput").send_keys(str(query))
-        browser.find_element_by_id("searchboxinput").send_keys(Keys.ENTER)
-        time.sleep(5)
-        data = browser.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div")
-        html_code =BeautifulSoup(browser.page_source, 'lxml')
-        #data_tag_code =   html_code.find_all("div", class_="section-layout section-scrollbox scrollable-y scrollable-show section-layout-flex-vertical")
-        data_tag_code =   html_code.find_all("div", class_="section-result")
-        #print(data_tag_code1)
-        global searchresults 
-        searchresults = list()
-        for i in data_tag_code:
-            if(i["aria-label"]):
-                searchresults.append(i["aria-label"])
-        return searchresults
-
+        pass
+         
     except Exception as e:
             print(e)
             details = ["details not found"]
             return details
+
+
+def get_details(q):
+    try:
+        places_data = gmaps.places(query=str(q))
+        details = list()
+        for i in places_data["results"]:
+            ref = i["reference"]
+            data = requests.get(f'https://maps.googleapis.com/maps/api/place/details/json?placeid={ref}&key=AIzaSyCFUBzYLW1sVSF2Q8laM_Sbo9JhOKqYiU0')
+            data = data.json()
+            data = data["result"]
+            details.append(i["name"] ,data["formatted_phone_number"])  
+            break
+        return details
+    except Exception as e:
+            print(e)
+            details = ["details not found"]
+            return details
+
+
